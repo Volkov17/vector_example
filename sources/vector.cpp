89fa6523313c42573e62f2ddbed9d38e5ba3a0e1
Volkov17 <32384533+Volkov17@users.noreply.github.com>
2018-03-23 14:23:49 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -5,42 +5,116 @@
 
 vector_t::vector_t()
 {
+	elements_(nullptr), size_(0), capacity_(0);
 }
 
 vector_t::vector_t(vector_t const & other)
 {
+	elements_ = new int [other.capacity_];
+	for ( int i =0; i<other.size_; i++)
+		elements_[i] = other.elements_[i];
+	size_ = other.size_;
+ 	capacity_ = other.capacity_;
+	
 }
 
 vector_t & vector_t::operator =(vector_t const & other)
 {
-	return *this;// hi
+	if ( this!= &other){
+		if (elemets_!=nullptr){
+			delete [] elements_l;
+		}
+		elements_=new int[other.capacity];
+		for (int i=0; i<other.size_;i++)
+			elements_[i] = other.elements_[i];
+		size_ = other.size_;
+ 		capacity_ = other.capacity_;
+	}
+	return *this;
 }
 
 bool vector_t::operator ==(vector_t const & other) const
 {
-	return false;
+	if ((size_ == other.size_) && (capacity_ == other.capacity_)){
+		for (int i=0;i<other.size_;i++){
+			if (elements_[i] == other.elements_[i]){
+				return false; 
+			}
+			else 
+				return true;
+		}
+	}
+	else 
+		return false;
+						
 }
 
 vector_t::~vector_t()
 {
+	delete [] elements_;
 }
 
 std::size_t vector_t::size() const
 {
-    return 0;
+    return size();
 }
 
 std::size_t vector_t::capacity() const
 {
-    return 0;
+    return capacity();
 }
 
 void vector_t::push_back(int value)
 {
+	if (capacity_=0){
+		capacity_=1;
+		size_=1;
+		elements_=new int [capacity];
+		elements_[size-1]=value;
+	}
+	else 
+	{
+		if (capacity_=size_){
+		capacity_ = 2 * capacity_;
+ 		data_ = new int [capacity_];
+ 		for (int i = 0;i < size_;i++ )
+ 		{
+ 			this->data_[i]=copy.data_[i]
+		}
+		size_++;		
+ 		data_[size_ - 1] = value;
+ 	}
+ 	else
+ 	{
+ 		size_++;
+ 		data_[size_ - 1] = value;
+ 	}
 }
 
 void vector_t::pop_back()
 {
+	if (size_)
+	{
+		size_--;
+		if (size_ <= ( capacity_ / 4 ))
+		{
+			capacity_ = capacity_ / 2;
+			int * el = new int [capacity_];
+			for (size_t i = 0; i < size_; i++)
+			{
+				el[i] = elements_[i];
+			}
+			
+			delete[]elements_;
+
+			elements_ = el;
+		}
+	}
+	else
+	{
+		std::cout << "empty";
+	}
+	
 }
 
 int & vector_t::operator [](std::size_t index)
