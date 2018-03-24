c4a701a5e7e871dc52d8b17741a17b7cebab16ad
Volkov17 <32384533+Volkov17@users.noreply.github.com>
2018-03-24 14:36:03 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -37,18 +37,12 @@ vector_t & vector_t::operator =(vector_t const & other)
 
 bool vector_t::operator ==(vector_t const & other) const
 {
-	if ((size_ == other.size_) && (capacity_ == other.capacity_)){
-		for (int i=0;i<other.size_;i++){
-			if (elements_[i] == other.elements_[i]){
-				return false; 
-			}
-			else 
-				return true;
-		}
-	}
-	else 
-		return false;
-						
+	if (size_ != other.size_) 
+	  return false;  
+	for (int i = 0; i < size_; i++) 
+		if (elements_[i] != other.elements_[i]) 
+			return false;
+	return true;					
 }
 
 vector_t::~vector_t()
@@ -77,6 +71,8 @@ void vector_t::push_back(int value)
 	else 
 	{
 		if (capacity_==size_){
+		vector_t copy( *this );
+ 		delete [] elements_;
 		capacity_ = 2 * capacity_;
  		elements_ = new int [capacity_];
  		for (int i = 0;i < size_;i++ )
